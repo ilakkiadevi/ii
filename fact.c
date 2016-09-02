c259f334ce8c9fff1dfaf8a0e25ef071093d97c7
ilakkiadevi <kilakkiaspm@gmail.com>
2016-09-02 11:59:07 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,fact=1,result;
+printf("enter the number");
+scanf(%d",n);
+for(i=1;i<n;i++)
+{
+result=fact*i;
+}
+printf("%d",result);
+}
